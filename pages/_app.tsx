5ba9f2539f56b9bd7aa26c305d3c0263ed1b6439
MateoNav26 <matenavarrete@gmail.com>
2025-07-12 15:56:57 -0300

    feat: setup global styling and brand theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
-import "@/styles/globals.css";
+import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { DM_Sans, Playfair_Display } from "next/font/google";
+
+const dmSans = DM_Sans({
+  subsets: ["latin"],
+  variable: "--font-dm-sans",
+  display: "swap",
+});
+const playfairDisplay = Playfair_Display({
+  subsets: ["latin"],
+  variable: "--font-playfair-display",
+  display: "swap",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={dmSans.variable + " " + playfairDisplay.variable + " font-sans"}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
