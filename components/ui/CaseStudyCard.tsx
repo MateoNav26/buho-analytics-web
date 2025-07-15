9b9101d431937503810e835446840d070518c5bd
MateoNav26 <matenavarrete@gmail.com>
2025-07-15 16:24:58 -0300

    feat: Styling Case Study

--- components/ui/CaseStudyCard.tsx
+++ components/ui/CaseStudyCard.tsx
@@ -21,14 +21,14 @@ export default function CaseStudyCard({
       <DirectionAwareHover
         imageUrl={backgroundImage}
         className="h-full w-full"
-        childrenClassName="flex flex-col justify-end p-6"
+        childrenClassName="flex flex-col justify-end pt-6 pb-3 px-6"
       >
-        <div className="absolute inset-0 bg-black/60 rounded-xl flex flex-col justify-end p-6">
+        <div className="absolute inset-0 bg-black/60 rounded-xl flex flex-col justify-end pt-6 pb-3 px-6 items-center text-center">
           <h3 className="font-serif text-2xl font-bold text-white-raw mb-2">
             {title}
           </h3>
           <span
-            className="font-sans text-base font-semibold text-black bg-primary-accent rounded-full px-4 py-2 mb-2 inline-block self-start shadow-lg border-2 border-primary-accent/80 max-w-md break-words text-left whitespace-pre-line"
+            className="font-sans text-base font-semibold text-black bg-primary-accent rounded-full px-4 py-2 mb-2 inline-block shadow-lg border-2 border-primary-accent/80 max-w-md break-words whitespace-pre-line"
           >
             {result}
           </span>
